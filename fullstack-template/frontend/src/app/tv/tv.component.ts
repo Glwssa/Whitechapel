ac612c4ed9e1260aa0b53f5348e1ee75c6bcfca0
stratos sikelis <strsikel@yahoo.gr>
2019-11-15 14:39:44 +0200

    tv general layout demo

--- fullstack-template/frontend/src/app/tv/tv.component.ts
+++ fullstack-template/frontend/src/app/tv/tv.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ami-fullstack-tv',
   templateUrl: './tv.component.html',
-  styleUrls: ['./tv.component.scss']
+  styleUrls: ['./tv.component.scss'],
+
 })
 export class TvComponent implements OnInit {
+  image: string;
+  round: number;
 
-  constructor() { }
+  constructor() {
+    this.image = 'https://www.w3schools.com/images/w3schools_green.jpg';
+    this.round = 1;
+   }
+
 
   ngOnInit() {
   }
 
+  increment() {
+    ++this.round;
+  }
+  deincrement() {
+    --this.round;
+  }
 }
+
+
