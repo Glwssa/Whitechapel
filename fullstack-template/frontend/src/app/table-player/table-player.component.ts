c8e04ea5c6d7746d9d3ddcecbba1fc14a5436bcf
Glwssa <46265146+Glwssa@users.noreply.github.com>
2019-11-17 15:29:38 +0200

    table player giblet setup with visible boolean

--- fullstack-template/frontend/src/app/table-player/table-player.component.ts
+++ fullstack-template/frontend/src/app/table-player/table-player.component.ts
@@ -9,13 +9,34 @@ export class TablePlayerComponent implements OnInit {
   player_image: string;
   gibbet: string;
   player_name: string;
+  gibvis1: boolean;
+  gibvis2: boolean;
+  gibvis3: boolean;
+  gibvis4: boolean;
+  gibvis5: boolean;
+  gibvis6: boolean;
+  gibvis7: boolean;
   constructor() {
     this.player_image = "https://cdn2.iconfinder.com/data/icons/business-management-52/96/Artboard_20-512.png";
     this.gibbet = "https://www.svgrepo.com/show/585/gibbet.svg";
     this.player_name = "Andreas";
+    this.gibvis1 = true;
+    this.gibvis2 = true;
+    this.gibvis3 = true;
+    this.gibvis4 = true;
+    this.gibvis5 = true;
+    this.gibvis6 = true;
+    this.gibvis7 = true;
    }
 
   ngOnInit() {
   }
 
+  set_visible_giblet(){
+
+  }
+
+  reset_giblet(){
+    this.gibvis1 = false;
+  }
 }
