fd1922a21ce83db94dca34cdf3e7c3bb24b01bc2
Glwssa <46265146+Glwssa@users.noreply.github.com>
2019-11-18 15:20:18 +0200

    table titles (starting and round titles) + player upvote and reset giblets

--- fullstack-template/frontend/src/app/table/table.component.ts
+++ fullstack-template/frontend/src/app/table/table.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { TablePlayerComponent } from '../table-player';
 
 
 @Component({
   selector: 'team13-Table',
+  providers:[TablePlayerComponent],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
   Background: string;
+  game_title_vis: boolean;
+  player1: boolean;
+  title: string;
+  titlevis: boolean;
 
   constructor() { 
     this.Background = 'https://i.imgur.com/UwNfp80.png';
+    this.title = "DEBATE"
+    this.titlevis = false;
+    this.game_title_vis = true;
+    this.player1 = true;
+    //this.change_title("ABILITY USE")
+    //this.chang_background_night();
+    //this.setup_table();
+    //reset_giblets();
+    
   }
 
   ngOnInit() {
@@ -23,6 +38,17 @@ export class TableComponent implements OnInit {
 
   chang_background_night(){
     this.Background = 'https://i.imgur.com/fKYhJzQ.png';
+  }
+  
+  change_title(title: string){
+    this.title = title;
+  }
+  
+  setup_table(){
+    this.titlevis = true;
+    this.game_title_vis = false;
+    this.player1 = true;
+    this.change_background_day()
   }
 
 }
